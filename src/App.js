26968319af8075cf364939042341ae14453e4697
Shubha R <shubha180396@gmail.com>
2025-12-20 22:38:04 +0530

    index.css change

--- src/App.js
+++ src/App.js
@@ -3,7 +3,9 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-     Shubha
+     <h1 className="text-3xl font-bold underline text-green-800 text-center m-4 p-4">
+      Hello world!
+    </h1>
     </div>
   );
 }
